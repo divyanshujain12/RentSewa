a672eea02a164acff416f5806bcf18259752b00d
divyanshuPC <divyanshujain12@hotmail.com>
2017-04-07 00:41:23 +0530

    api integration home

---
+++ app/src/main/java/com/example/divyanshujain/rentsewa/VendorAllProductListing.java
@@ -0,0 +1,14 @@
+package com.example.divyanshujain.rentsewa;
+
+import android.os.Bundle;
+
+import com.example.divyanshujain.rentsewa.GlobalClasses.BaseActivity;
+
+public class VendorAllProductListing extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_all_product_listing);
+    }
+}
